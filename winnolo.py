5b82081cda49fc81ed54823d06b20a1a830284aa
Thiago Franco de Moraes <totonixsame@gmail.com>
2020-04-29 17:26:45 -0300

    Calling pip

--- winnolo.py
+++ winnolo.py
@@ -8,6 +8,22 @@ from urllib import request
 from zipfile import ZipFile
 
 
+def run_cmd(cmd, env=None, cwd=None):
+    with subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        bufsize=1,
+        universal_newlines=True,
+        env=env,
+        cwd=cwd,
+    ) as p:
+        for line in p.stdout:
+            print(line, end="")
+
+    if p.returncode != 0:
+        raise subprocess.CalledProcessError(p.returncode, p.args)
+
+
 def download(url):
     tmp_folder = pathlib.Path(tempfile.mkdtemp())
     filename = url.split("/")[-1]
@@ -20,12 +36,14 @@ def download(url):
 
 
 def extract_zip(filepath, dest):
-    with ZipFile(filepath, 'r') as zip_obj:
-       zip_obj.extractall(dest)
+    with ZipFile(filepath, "r") as zip_obj:
+        zip_obj.extractall(dest)
 
 
 def get_python(version, dest, arch="amd64"):
-    url = f"https://www.python.org/ftp/python/{version}/python-{version}-embed-{arch}.zip"
+    url = (
+        f"https://www.python.org/ftp/python/{version}/python-{version}-embed-{arch}.zip"
+    )
     filepath = download(url)
     extract_zip(filepath, dest)
 
@@ -55,6 +73,18 @@ def main():
 
     # Copying the project inside dist_folder
     copy_tree(str(project_folder), str(app_folder))
+
+    # Installing python libs from requirements
+    run_cmd(
+        [
+            str(python_folder.joinpath("python.exe")),
+            "-m",
+            "pip",
+            "install",
+            "-r",
+            str(app_folder.joinpath("requirements.txt")),
+        ]
+    )
 
 
 if __name__ == "__main__":
